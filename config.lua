af3fe0d785f349ed081aaf3ae32bc07f74a8f8be
dastagg:opus5 <dastagg@stagglinux.com>
2022-12-23 18:38:30 -0500

    Remove ftplugin, move config to lua/user.

--- config.lua
+++ config.lua
@@ -87,41 +87,15 @@ require("lvim.lsp.manager").setup("pylsp", opts)
 --   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 -- end
 
--- -- set a formatter, this will override the language server formatting capabilities (if it exists)
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  -- { command = "black", filetypes = { "python" } },
-  -- { command = "isort", filetypes = { "python" } },
-  {
-    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-    command = "prettier",
-    ---@usage arguments to pass to the formatter
-    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    extra_args = { "--print-with", "100" },
-    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-    filetypes = { "typescript", "typescriptreact" },
-  },
-}
+-- set a formatter, this will override the language server formatting capabilities (if it exists)
+require("user.formatters")
 
 -- set additional linters
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
--- --   {
--- --     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
--- --     command = "shellcheck",
--- --     ---@usage arguments to pass to the formatter
--- --     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
--- --     extra_args = { "--severity", "warning" },
--- --   },
--- --   {
--- --     command = "codespell",
--- --     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
--- --     filetypes = { "javascript", "python" },
--- --   },
--- }
+require("user.linters")
 
+-- set additional plugins
 require "user.plugins"
+
 -- Setup dap for python
 lvim.builtin.dap.active = true
 local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
